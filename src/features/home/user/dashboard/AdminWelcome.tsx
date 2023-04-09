2b1b7eca64fcabc369ce40db7cacb770b707f97f
Pierre Perrin <pierre.perrin@epitech.eu>
2023-04-09 18:32:12 +0100

    [ADD] Admin welcome page v1

--- src/features/home/user/dashboard/AdminWelcome.tsx
+++ src/features/home/user/dashboard/AdminWelcome.tsx
@@ -3,14 +3,30 @@ import { Divider } from '@mantine/core';
 import Banner from './components/AdminWelcome/Banner';
 import Setup from './components/AdminWelcome/Setup';
 
+import { useListUsers } from 'hooks/api/useListUsers';
+import CustomLoader from 'components/loader/CustomLoader';
+import { useListParts } from 'hooks/api/useListParts';
+import { useGetConfig } from 'hooks/api/useGetConfig';
+import { useListSprints } from 'hooks/api/useListSprints';
+
 export function AdminWelcome() {
-    return (
+    const { data: userList } = useListUsers(true);
+    const { data: partList } = useListParts(true);
+    const { data: sprintsList } = useListSprints();
+    const { data: config } = useGetConfig();
+
+    return userList && partList && config && sprintsList ? (
         <div style={{padding: 20}}>
             <Banner />
             <Divider style={{marginTop: 20}} />
-            <Setup />
+            <Setup
+                configSetup={config.config.Default_Password.value !== null}
+                userSetup={userList.length > 1}
+                partSetup={partList.length > 0}
+                sprintsSetup={sprintsList.length > 0}
+            />
         </div>
-    );
+    ) : <></>;
 }
 
 export default AdminWelcome;
--- src/features/home/user/dashboard/components/AdminWelcome/Setup.tsx
+++ src/features/home/user/dashboard/components/AdminWelcome/Setup.tsx
@@ -5,21 +5,33 @@ import { useSetupStyle } from "../../styles/useSetupStyle";
 import { HiOutlineCog6Tooth } from "react-icons/hi2"
 import { FiUsers, FiCheckSquare } from "react-icons/fi";
 import { BsCalendar2Week } from "react-icons/bs"
+import { useState } from "react";
 
-const Setup = () => {
-  const { classes } = useSetupStyle();
+interface Props {
+    configSetup: boolean;
+    userSetup: boolean;
+    partSetup: boolean;
+    sprintsSetup: boolean;
+}
 
-  return (
-    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-      <Title className={classes.title}>Setup <span className={classes.span}>Roadmap</span></Title>
-      <Stepper active={3} allowNextStepsSelect={false} radius={"md"} size="xl" breakpoint={"xl"} mt={40} completedIcon={<FiCheckSquare size={"1.6rem"} />}>
-        <Stepper.Step state="stepCompleted" icon={<HiOutlineCog6Tooth size="1.6rem" />} label={<Text size={"md"} weight={"bold"}>Config</Text>} description={<Text size={"sm"}> <Anchor href="/home/admin/config">Configure</Anchor> your instance</Text>} />
-        <Stepper.Step state="stepCompleted" icon={<FiUsers size="1.4rem" />} label={<Text size={"md"} weight={"bold"}>Users</Text>} description={<Text size={"sm"}>Create your team <Anchor href="/home/editor/users">users</Anchor></Text>} />
-        <Stepper.Step state="stepInactive" icon={<FiUsers size="1.4rem" />} label={<Text size={"md"} weight={"bold"}>Parts</Text>} description={<Text size={"sm"}>Create your project <Anchor href="/home/editor/parts">parts</Anchor></Text>} />
-        <Stepper.Step state="stepInactive" icon={<BsCalendar2Week size="1.4rem" />} label={<Text size={"md"} weight={"bold"}>Sprint</Text>} description={<Text size={"sm"}>Create your first <Anchor href="/home/admin/sprints">sprint</Anchor></Text>} />
-      </Stepper>
-    </div>
-  );
+const Setup: React.FC<Props> = ({ configSetup, userSetup, partSetup, sprintsSetup }) => {
+    const { classes } = useSetupStyle();
+    
+    const active = (sprintsSetup ? 4 : partSetup ? 3 : userSetup ? 2 : configSetup ? 1 : 0);
+
+    console.log(userSetup)
+
+    return (
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+        <Title className={classes.title}>Setup <span className={classes.span}>Roadmap</span></Title>
+        <Stepper active={active} allowNextStepsSelect={false} radius={"md"} size="xl" breakpoint={"xl"} mt={40} completedIcon={<FiCheckSquare size={"1.6rem"} />}>
+            <Stepper.Step state="stepCompleted" icon={<HiOutlineCog6Tooth size="1.6rem" />} label={<Text size={"md"} weight={"bold"}>Config</Text>} description={<Text size={"sm"}> <Anchor href="/home/admin/config">Configure</Anchor> your instance</Text>} />
+            <Stepper.Step state="stepCompleted" icon={<FiUsers size="1.4rem" />} label={<Text size={"md"} weight={"bold"}>Users</Text>} description={<Text size={"sm"}>Create your team <Anchor href="/home/editor/users">users</Anchor></Text>} />
+            <Stepper.Step state="stepInactive" icon={<FiUsers size="1.4rem" />} label={<Text size={"md"} weight={"bold"}>Parts</Text>} description={<Text size={"sm"}>Create your project <Anchor href="/home/editor/parts">parts</Anchor></Text>} />
+            <Stepper.Step state="stepInactive" icon={<BsCalendar2Week size="1.4rem" />} label={<Text size={"md"} weight={"bold"}>Sprint</Text>} description={<Text size={"sm"}>Create your first <Anchor href="/home/admin/sprints">sprint</Anchor></Text>} />
+        </Stepper>
+        </div>
+    );
 }
 
 export default Setup;
