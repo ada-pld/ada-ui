0cabf102db1b8539969efa3d9ef92cd4dfaa4429
Pierre Perrin <pierre.perrin@epitech.eu>
2023-05-04 20:47:13 +0100

    [FIX] playwright test command config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,48 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  projects: [
+    {
+      name: 'Chromium',
+      use: {
+        browserName: 'chromium',
+        headless: true,
+      },
+    },
+    {
+      name: 'Firefox',
+      use: {
+        browserName: 'firefox',
+        headless: true,
+      },
+    },
+    {
+      name: 'WebKit',
+      use: {
+        browserName: 'webkit',
+        headless: true,
+      },
+    },
+  ],
+  use: {
+    screenshot: 'only-on-failure',
+    trace: 'retain-on-failure',
+    video: 'retry-with-video',
+  },
+  reporter: [
+    ['dot'],
+    [
+      'html',
+      {
+        outputFile: 'playwright-report/report.html',
+      },
+    ],
+  ],
   webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:3000',
+    command: 'npm run test:start',
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
   },
 };
 
