846f0b3675dcfb8b580e323db1d0ee7f3cab13c2
Pierre Perrin <63340473+protoxvga@users.noreply.github.com>
2023-05-04 13:59:41 +0100

    [ADD] Testing workflow (#69)

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,17 +1,15 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
 
 test('Login page rendering', async ({ page }) => {
-    // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-    await page.goto('http://localhost:3000/login');
-    await expect(page.locator('h1')).toContainText('Welcome back !');
-})
+  await page.goto('/login');
+  await expect(page.locator('h1')).toContainText('Welcome back !');
+});
 
 test('Login with invalid credentials', async ({ page }) => {
-    // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-    await page.goto('http://localhost:3000/login');
-    await page.getByLabel('Email').fill('test@test.fr');
-    await page.getByLabel('Password').fill('Test.147');
-    await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.goto('/login');
+  await page.getByLabel('Email').fill('test@test.fr');
+  await page.getByLabel('Password').fill('Test.147');
+  await page.getByRole('button', { name: 'Sign in' }).click();
 
-    await expect(page.getByText('Invalid credentials')).toBeVisible();
-})
+  await expect(page.getByText('Invalid credentials')).toBeVisible();
+});
