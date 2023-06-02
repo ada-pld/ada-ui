7b7939dbeca52904809e6d221071023ecdded5c6
Pierre Perrin <pierre.perrin@epitech.eu>
2023-06-02 23:30:41 +0100

    [ADD] Favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
     return (
         <Html lang="en">
         <Head>
+            <link rel="icon" href="/favicon.ico" sizes="any" />
             <meta name="robots" content="noindex, nofollow" />
         </Head>
         <body>
